d4de11b27e0ed9593c14bd6a319aba7f587f918f
yonvela20 <yon.vela20@gmail.com>
2018-10-01 20:53:12 +0200

    Intentado el control de errores de la contraseña

--- Formulario/formulario.php
+++ Formulario/formulario.php
@@ -13,18 +13,32 @@ $nombre="";
 $edad=0;
 $email="";
 $error=[];
+$contrasena="";
 
 if(isset($_POST['envio'])){
+
     //Validamos nombre
     if (!isset($_POST['nombre']) or !$_POST['nombre']) {
-        $error['nombre']= "Error: el nombre es requerido.";
+        $error['nombre']= "Error: el nombre es requerido."; 
     } else {
-    $nombre=$_POST['nombre'];
-        if(strlen($nombre)<3) 
+        $nombre=$_POST['nombre'];
+        if (strlen($nombre)<3){ 
             $error['nombre']='La longitud mínima es de tres caracteres';
-        if(is_numeric($nombre))
-            $error['nombre']='¿Tienes número de serie?';
         }
+        if (is_numeric($nombre)){
+            $error['nombre']='¿Tienes número de serie?';
+        }    
+    }
+        
+    //Validamos la contraseña    
+    if (!isset($_POST['contrasena']) or !$_POST['contrasena']){
+        $error['contrasena']= "Error: la contraseña es requerida";
+    } else{
+        $contrasena=$_POST['contrasena'];
+        if(strlen($contrasena)<6){
+            $error['contrasena']="La contraseña debe tener minimo 6 caracteres";
+        }       
+    }
         
     if(!count($error)){
         //Guardaríamos en la BD    
