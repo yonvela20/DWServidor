a4ea455cb1baf1dc59a9da2422acc2df75abd4e3
yonvela20 <yon.vela20@gmail.com>
2018-11-27 12:04:10 +0100

    Mastermind practicamente acabado

--- Mastermind/index.php
+++ Mastermind/index.php
@@ -6,18 +6,25 @@ session_start();
 if(!isset($_SESSION['master'])){
     $mastermind = new Mastermind();
     $mastermind->inicioJuego();
+    var_dump($mastermind->getRandom());
     $_SESSION['master']=$mastermind;
 } else{
-    $mastermid=$_SESSION['master'];
-
+    $mastermind=$_SESSION['master'];
+    var_dump($mastermind->getRandom());
 } 
 
 if(isset($_POST['num'])){
-
     $num = $_POST['num'];
-    $mastermid->validar($num);
-
+    $mastermind->validar($num);
 }
+
+var_dump("Muertos: ".$mastermind->getMuertos());
+var_dump("Heridos: ".$mastermind->getHeridos());
+var_dump("Intentos: ".$mastermind->getIntentos());
+var_dump("Ultima jugada: ".$mastermind->getUltimaJugada());
+
+$mastermind->setMuertosCero();
+$mastermind->setHeridosCero();
 
 require "vista.php";
 
--- Mastermind/mastermind.php
+++ Mastermind/mastermind.php
@@ -13,9 +13,9 @@ class Mastermind{
     ];
 
     public $numero;
-   
+    
+    //Genera un array de cuatro valores aleatorios del 1 al 6 sin repeticiones 
     public function inicioJuego(){
-        //generaRandom($this->random);
         $rndInicial = mt_rand(1, 6); 
         array_push($this->random, $rndInicial); //Metemos un primer random en el array
 
@@ -27,27 +27,77 @@ class Mastermind{
                 array_push($this->random, $rnd); //Si no lo está lo metemos dentro
             }
         }
-        var_dump($this->random);
+        return $this->random;
     }
 
+    //Funcion provisional para el desarrollo
+    public function getRandom(){
+        return $this->random;
+    }
+
+    //Funcion que comprueba si la jugada esta hecha y si no comprueba cuantos numeros has acertado
     public function validar($numero){
+        if(in_array($numero, $this->jugadas['num'])){
+            //TODO: Aqui va la funcion de error que hay que hacer 
+            var_dump("No puedes repetir jugada");
+        } else{
+            $this->compruebaNumero($numero);
+
+            if($this->jugadas['muertos'] >= 4){
+                //Aqui te debe redireccionar a una pagina de ganar, esto es provisional
+                var_dump("Has ganado");
+            }
+            if($this->jugadas['muertos'] < 4){
+                return "";
+            }
+        }
+    }
+
+    //Funcion que comprueba los numeros introducidos
+    public function compruebaNumero($numero){
+        //Estas dos lineas son de desarrollo se quitarán mas adelante
         var_dump("Numeros a validar: ");
         var_dump($numero);
 
         for($i = 0; $i < count($this->random); $i++){
-            
-            array_push($this->jugadas['num'], $numero);
-            
-            if($this->random[$i] == $numero[$i]){
-                $this->jugadas['muertos']++;
-                var_dump("El ".$i." es correcto");
+
+            if(in_array($this->random[$i], $numero)){
+                $this->jugadas['heridos']++;
+                if($this->random[$i] == $numero[$i]){
+                    $this->jugadas['muertos']++;
+                    $this->jugadas['heridos']--;
+                }
             }
         }
-
+        
+        array_push($this->jugadas['num'], $numero);
         $this->jugadas['intentos']++;
+    }
 
-        var_dump($this->jugadas['muertos']);
-        var_dump($this->jugadas['intentos']);
+    //Gettters y setters a cero
+    public function getMuertos(){
+        return $this->jugadas['muertos']; 
+    }
+
+    public function setMuertosCero(){
+        $this->jugadas['muertos'] = 0; 
+    }
+
+    public function getHeridos(){
+        return $this->jugadas['heridos'];
+    }
+
+    public function setHeridosCero(){
+        $this->jugadas['heridos'] = 0;
+    }
+
+    public function getIntentos(){
+        return $this->jugadas['intentos'];
+    }
+
+    public function getUltimaJugada(){
+        $ultimaJugada = count($this->jugadas['num']) - 1;
+        return array_slice($this->jugadas['num'], $ultimaJugada);
     }
 }
 
